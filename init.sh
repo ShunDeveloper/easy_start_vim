e8f46797dc2625c26fb5f8947869ad783b2abb66
ShunDeveloper <shunsuke.tsuchiya@prtimes.co.jp>
2023-04-16 21:19:56 +0900

    コミット忘れ

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p ~/.vim
+cp -r config ~/.vim/config
 cp .vimrc ~/.vimrc
+
+# if you do not have Plug, please use this curl command
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
